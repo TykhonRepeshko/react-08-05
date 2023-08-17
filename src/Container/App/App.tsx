28b928276f76d4ed82ad7014783937aae18611de
Tykhon <repeshko.t@gmail.com>
2023-08-17 22:16:33 +0300

    added header

---
+++ src/Container/App/App.tsx
@@ -0,0 +1,13 @@
+import Header from '../Header/header'
+import CssBaseline from '@mui/material/CssBaseline'
+
+type Props = {}
+const App = (props: Props) => {
+    return (
+        <>
+            <CssBaseline />
+            <Header />
+        </>
+    )
+}
+export default App
